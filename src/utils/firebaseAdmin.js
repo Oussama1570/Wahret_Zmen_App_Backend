b3a1351ba1626f5c44a3dd472d3d08d30b95ac21
Oussama <Messamessus@yahoo.com>
2025-03-27 16:18:15 +0000

    update index url

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,10 +1,17 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+// src/utils/firebaseAdmin.js
 
+const admin = require('firebase-admin');
 
+// Get the key from the environment variable
+const firebaseKey = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
+
+// Fix the private_key: replace \\n with actual newlines
+firebaseKey.private_key = firebaseKey.private_key.replace(/\\n/g, '\n');
+
+// Initialize Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(firebaseKey),
   });
 }
 
