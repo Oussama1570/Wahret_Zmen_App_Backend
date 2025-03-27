54fef23b2bee5a1f6ef3cf5e54482b8300656b7b
Oussama <Messamessus@yahoo.com>
2025-03-27 16:37:14 +0000

    update

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
   : [
       "http://localhost:5173",
+     "https://wahret-zmen-app-frontend.vercel.app",
       
     ];
 
